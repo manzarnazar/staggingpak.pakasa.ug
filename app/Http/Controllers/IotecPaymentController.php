ead0f3d0ad03a91fd0aba65811503957a75faedb
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-12 22:01:38 +0500

    test

--- app/Http/Controllers/IotecPaymentController.php
+++ app/Http/Controllers/IotecPaymentController.php
@@ -111,7 +111,7 @@ public function checkPaymentStatus($transactionId, $package_id)
         $responseData = $response->json();
 
         // Check if the payment was successful
-        if ($response->successful() && $responseData['statusCode'] === 'success') {
+        if ($response->successful() && $responseData['status'] === 'success') {
             // Assign the package to the user
             $assignPackageResponse = $this->assignPackage($transactionId, $user->id, $package_id);
 
